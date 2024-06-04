af914bc61a26f8104d72fdcd4b8cf2eae3241906
ame <elliotdtx@gmail.com>
2024-06-04 17:34:23 -0500

    fuck nix

--- configurationSymlink.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -xe
-
-ln -s -f ~/dotfiles/ ~/.config/ && rename dotfiles home-manager ~/.config/dotfiles
-sudo ln -sf $(find ~/dotfiles/ | grep "configuration.nix") /etc/nixos/configuration.nix
