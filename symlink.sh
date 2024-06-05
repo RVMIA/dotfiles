bc0db1d06976fb9be05e4e50e684e076cf805be9
ame <elliotdtx@gmail.com>
2024-06-04 23:54:12 -0500

    dunstrc

--- symlink.sh
+++ symlink.sh
@@ -10,6 +10,7 @@ ln -s -T /home/ame/dotfiles/nvim ~/.config/nvim
 ln -s -T /home/ame/dotfiles/zathura ~/.config/zathura
 ln -s -T /home/ame/dotfiles/neofetch ~/.config/neofetch
 ln -s -T /home/ame/dotfiles/sway ~/.config/sway
+ln -s -T /home/ame/dotfiles/dunst ~/.config/dunst
 
 ln -s -T /home/ame/dotfiles/.Xdefaults ~/.Xdefaults
 doas ln -s -T /home/ame/dotfiles/dmenu-5.3 /etc/portage/savedconfig/x11-misc/dmenu-5.3
