3075b1de4cc0a32821ce271b0bbd8627b5b27e10
ame <elliotdtx@gmail.com>
2024-07-17 15:09:24 -0500

    new dock screenlayout

--- scripts/screenlayout.sh
+++ scripts/screenlayout.sh
@@ -3,16 +3,31 @@ HOST=$(hostname)
 XRANDR=$(xrandr -q)
 
 if [[ $HOST = "ame" ]]; then
-    xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal &
+    xrandr \ 
+        --output DisplayPort-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
+        --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal 
 elif [[ $HOST = "ameFramework" ]]; then
     if xrandr -q | grep -q "DisplayPort-2 connected"; then
-        xrandr --output eDP --primary --mode 2256x1504 --pos 206x1502 --rotate normal --output DisplayPort-2 --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.39
-    elif xrandr -q | grep -q "DisplayPort-2 disconnected"; then 
-                    xrandr --output DisplayPort-2 --off
-    fi
+        xrandr \
+            --output eDP --primary --mode 2256x1504 --pos -168x1080 --rotate normal \
+            --output DisplayPort-2 --mode 1920x1080 --pos 0x0 --rotate normal
+
+    elif xrandr -q | grep -q "DisplayPort-8 connected"; then 
+        xrandr \
+            --output DisplayPort-8 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
+            --output DisplayPort-9 --mode 1920x1080 --pos 0x0 --rotate normal \
+            --output eDP --off
+    else
+        xrandr \
+            --output eDP --mode 2256x1504 --pos 0x0 --rotate normal \
+            --output DisplayPort-2 --off \
+            --output DisplayPort-8 --off \
+            --output DisplayPort-9 --off
+    fi  
 else
     arandr &
     disown
 fi
+nitrogen --restore &
 
 
