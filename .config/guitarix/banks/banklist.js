afc9a3f0eafc7e555b76b63e354a63e3ae5c15b4
ame <elliotdtx@gmail.com>
2024-08-03 17:14:21 -0500

    gdbrc

--- .config/guitarix/banks/banklist.js
+++ .config/guitarix/banks/banklist.js
@@ -1,5 +1,5 @@
 [
-  ["Meshuggah", "Meshuggah.gx", 1, 0, [1, 2], 1722028175],
+  ["Meshuggah", "Meshuggah.gx", 1, 0, [1, 2], 1722568643],
   ["metalpack-v1", "metalpack-v1.gx", 1, 0, [1, 2], 1721248058],
   ["JP", "JP.gx", 1, 0, [1, 2], 1721248058],
   ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1721248058]
